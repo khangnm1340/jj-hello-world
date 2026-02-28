211213c4ec8c56de1d5f25802cc37e6c367aa028
Ngo Minh Khang <khangnm1340@ut.edu.vn>
2026-02-28 15:49:02 +0700

    create hello and goodbye functions

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,7 +1,16 @@
 /// A "Hello, world!" program.
-/// 
+///
 /// This is the best implementation of this program to ever exist.
 
 fn main() {
+    print_hello();
+    print_goodbye();
+}
+
+fn print_hello() {
     println!("Hello, world!");
+}
+
+fn print_goodbye() {
+    println!("Goodbye, world!");
 }
